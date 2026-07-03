ffd032af833cab4aed8564cb645740e85982016d
Dayo Thompson <dayo.thompson@yahoo.com>
2026-07-03 16:31:14 -0400

    Update server.py

--- server.py
+++ server.py
@@ -8,4 +8,4 @@ from yahoo_mail.mcp_server import mcp
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(mcp.sse_app(), host="0.0.0.0", port=port)
+    uvicorn.run(mcp.streamable_http_app(), host="0.0.0.0", port=port)
